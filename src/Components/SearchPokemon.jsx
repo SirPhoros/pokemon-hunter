a3303afd5f86a091fd7abe42ea34f44f4f5bbaa7
SirPhoros <cgtorrubia@gmail.com>
2023-08-24 22:18:52 +0100

    show array of pokemon

--- src/Components/SearchPokemon.jsx
+++ src/Components/SearchPokemon.jsx
@@ -2,33 +2,36 @@ import { useQuery, gql } from '@apollo/client'
 
 const EXAMPLE_POKEMON = gql`
 	{
-		getPokemon(pokemon: dragonite) {
+		getFuzzyPokemon(pokemon: "reun", take: 10, reverse: false) {
+			species
 			sprite
 			num
-			species
 		}
 	}
 `
+
 export default function SearchPokemon() {
-	const { loading, error, data } = useQuery(EXAMPLE_POKEMON)
+	const { data, loading, error } = useQuery(EXAMPLE_POKEMON)
 
 	if (loading) return <p>Loading...</p>
 
-	if (error) return <p>Error : {error.message}</p>
-	const { sprite, species, num } = data.getPokemon
+	if (error) return <p>Error: {error.message}</p>
 
 	return (
-		<>
-			{' '}
-			<h3>{species}</h3>
-			<img
-				width="400"
-				height="250"
-				alt="pokemon sprite"
-				src={`${sprite}`}
-			/>
-			<br />
-			<br />
-		</>
+		<div>
+			{data.getFuzzyPokemon.map((pokemon) => (
+				<div key={pokemon.num}>
+					<h3>{pokemon.species}</h3>
+					<img
+						width="400"
+						height="250"
+						alt="pokemon sprite"
+						src={pokemon.sprite}
+					/>
+					<br />
+					<br />
+				</div>
+			))}
+		</div>
 	)
 }
