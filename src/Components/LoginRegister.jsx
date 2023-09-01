1a3ae463ada859036df621c278b6b8549ff77e4e
SirPhoros <cgtorrubia@gmail.com>
2023-09-01 16:40:17 +0100

    provide login/register feedback to the user

--- src/Components/LoginRegister.jsx
+++ src/Components/LoginRegister.jsx
@@ -6,6 +6,7 @@ import { useUser } from '../context/UserContext'
 function LoginRegister() {
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
+	const [feedbackMessage, setFeedbackMessage] = useState('') // Feedback message state
 	const { setUserUID, setUsername, setIsLoggedIn } = useUser()
 
 	const handleLogin = () => {
@@ -21,12 +22,16 @@ function LoginRegister() {
 					// Successfully logged in, set user data as cookies
 					Cookies.set('userUID', user.uid)
 					Cookies.set('username', user.email.split('@')[0])
+					// Set success feedback message
+					setFeedbackMessage('Login successful!')
 				} else {
 					console.error('User is undefined')
 				}
 			})
 			.catch((error) => {
 				console.error(error.message)
+				// Set error feedback message
+				setFeedbackMessage('Login failed. Check your email and password.')
 			})
 	}
 
@@ -40,21 +45,29 @@ function LoginRegister() {
 					// Set the user's UID and username in the context
 					setUserUID(user.uid)
 					setUsername(user.email.split('@')[0])
-					// Successfully registered in, set user data as cookies
+					// Successfully registered, set user data as cookies
 					Cookies.set('userUID', user.uid)
 					Cookies.set('username', user.email.split('@')[0])
+					// Set success feedback message
+					setFeedbackMessage('Registration successful!')
 				} else {
 					console.error('User is undefined')
 				}
 			})
 			.catch((error) => {
 				console.error(error.message)
+				// Set error feedback message
+				setFeedbackMessage(
+					'Registration failed. Check your email and password.'
+				)
 			})
 	}
 
 	return (
 		<section className="login-register-container">
 			<h2>Login/Register</h2>
+			{/* Feedback message */}
+			{feedbackMessage && <p className="feedback-message">{feedbackMessage}</p>}
 			<input
 				type="text"
 				placeholder="Email"
