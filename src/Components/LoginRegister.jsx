0601e5886341d92f0764590a69e7302ccd61f70a
SirPhoros <cgtorrubia@gmail.com>
2023-09-01 15:03:12 +0100

    change username format

--- src/Components/LoginRegister.jsx
+++ src/Components/LoginRegister.jsx
@@ -17,10 +17,10 @@ function LoginRegister() {
 					setPassword('')
 					// Set the user's UID and username in the context
 					setUserUID(user.uid)
-					setUsername(user.email)
+					setUsername(user.email.split('@')[0])
 					// Successfully logged in, set user data as cookies
 					Cookies.set('userUID', user.uid)
-					Cookies.set('username', user.email)
+					Cookies.set('username', user.email.split('@')[0])
 				} else {
 					console.error('User is undefined')
 				}
@@ -39,10 +39,10 @@ function LoginRegister() {
 					setIsLoggedIn(true)
 					// Set the user's UID and username in the context
 					setUserUID(user.uid)
-					setUsername(user.email)
+					setUsername(user.email.split('@')[0])
 					// Successfully registered in, set user data as cookies
 					Cookies.set('userUID', user.uid)
-					Cookies.set('username', user.email)
+					Cookies.set('username', user.email.split('@')[0])
 				} else {
 					console.error('User is undefined')
 				}
