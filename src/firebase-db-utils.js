f48b63b2aab7f16af41675ea2856dd97bd1355b3
SirPhoros <cgtorrubia@gmail.com>
2023-09-18 15:13:02 +0100

    create .env file and hide sensitive information

--- src/firebase-db-utils.js
+++ src/firebase-db-utils.js
@@ -24,12 +24,12 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'AIzaSyBbhBbyh48g-ElQP9bVvWPDErOXRoId70I',
-	authDomain: 'pokemon-hunter-cc4b0.firebaseapp.com',
-	projectId: 'pokemon-hunter-cc4b0',
-	storageBucket: 'pokemon-hunter-cc4b0.appspot.com',
-	messagingSenderId: '150000503332',
-	appId: '1:150000503332:web:058b918744d3f03e4fe6b1',
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID,
 }
 
 //INITIALISE FIREBASE
